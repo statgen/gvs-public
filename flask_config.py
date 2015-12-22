96f285acf8018c2e0a5e165075a0df4b687da04e
Peter VandeHaar <pjvandehaar@gmail.com>
2015-12-21 22:25:25 -0500

    Google OAuth2 triggers the callback

--- flask_config.py
+++ flask_config.py
@@ -2,5 +2,5 @@ SECRET_KEY = 'development key'
 
 # Gotten from <https://console.developers.google.com/apis/credentials?project=genome-variant-server>
 # see the nice documentation at <https://support.google.com/cloud/answer/6158849?hl=en&ref_topic=6262490>
-GOOGLE_OAUTH2_CLIENT_SECRET_ = 'fake key'
-GOOGLE_OAUTH2_CLIENT_ID = 'fake id'
+GOOGLE_LOGIN_CLIENT_ID = 'fake key'
+GOOGLE_LOGIN_CLIENT_SECRET = 'fake id'

9603191a8431654422d8bfc8a162abfe7d4b23b6
Peter VandeHaar <pjvandehaar@gmail.com>
2016-02-10 05:00:20 -0500

    Fix problems with flask_config

--- flask_config.py
+++ flask_config.py
@@ -1,3 +1,5 @@
+# Note: Flask will only copy UPPERCASE variable names into `app.config`.
+
 import sys
 import os.path
 import glob
@@ -13,49 +15,81 @@ class BaseConfig(object):
     GOOGLE_LOGIN_CLIENT_ID = 'fake key'
     GOOGLE_LOGIN_CLIENT_SECRET = 'fake id'
 
-    administrators = [
+    ADMINS = [
         'pjvh@umich.edu',
         'dtaliun@umich.edu',
     ]
 
-    examples = {
-        'gene': 'APOL1',
-        'transcript': 'ENST00000407236',
-        'variant': '22-46615880-T-C',
-        'multi-allelic-variant': 'rs1800234',
-        'region': '22:46615715-46615880',
-    }
-
-    pass_only_variants = False
+    ONLY_PASS_VARIANTS = False
 
     LOAD_DB_PARALLEL_PROCESSES = 8
 
 class BravoConfig(BaseConfig):
-    name = 'Bravo'
-    dataset_name = 'TOPMED'
-    _release = 'freeze2'
-    num_samples = 6015
+    BROWSER_NAME = 'Bravo'
+    DATASET_NAME = 'TOPMED'
+    release = 'freeze2'
+    NUM_SAMPLES = 6015
 
-    mongo = {
+    MONGO = {
         'host': 'localhost',
-        'port': '27017',
+        'port': 27017,
         'name': 'topmed_freeze2',
     }
 
-    base_coverage = [
+    BASE_COVERAGE = [
         {
             'min-length-bp': 0,
             'max-length-bp': 5000,
-            'path': {str(i): '/var/browser_coverage/topmed_freeze2_random1000/full/{}.topmed_freeze2.coverage.full.json.gz'.format(i) for i in range(1,1+22)},
+            'path': {str(chrom): '/var/browser_coverage/topmed_freeze2_random1000/full/{chrom}.topmed_freeze2.coverage.full.json.gz'.format(chrom=chrom) for chrom in range(1,1+22)},
         },
         {
             'min-length-bp': 5000,
             'max-length-bp': sys.maxint,
-            'path': {str(i): '/var/browser_coverage/topmed_freeze2_random1000/bin_25e-2/{}.topmed_freeze2.coverage.bin_25e-2.json.gz'.format(i) for i in range(1,1+22)},
+            'path': {str(chrom): '/var/browser_coverage/topmed_freeze2_random1000/bin_25e-2/{chrom}.topmed_freeze2.coverage.bin_25e-2.json.gz'.format(chrom=chrom) for chrom in range(1,1+22)},
         },
     ]
 
     _FILES_DIRECTORY = '/var/imported/topmed_freeze2'
+    SITES_VCFS = glob.glob(os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'ALL.polymorphic.topmed_freeze2.vcf.gz')),
+    GENCODE_GTF = os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'gencode.gtf.gz'),
+    CANONICAL_TRANSCRIPT_FILE = os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'canonical_transcripts.txt.gz'),
+    OMIM_FILE = os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'omim_info.txt.gz'),
+    BASE_COVERAGE_FILES = glob.glob(os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'coverage', 'Panel.*.coverage.txt.gz')),
+    # How to get a snp141.txt.bgz file:
+    #   wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/database/snp141.txt.gz
+    #   zcat snp141.txt.gz | cut -f 1-5 | bgzip -c > snp141.txt.bgz
+    #   tabix -0 -s 2 -b 3 -e 4 snp141.txt.bgz
+
+    #   wget ftp://ftp.ncbi.nlm.nih.gov/snp/organisms/human_9606_b142_GRCh37p13/database/organism_data/b142_SNPChrPosOnRef_105.bcp.gz
+    #   zcat b142_SNPChrPosOnRef_105.bcp.gz | awk '$3 != ""' | perl -pi -e 's/ +/\t/g' | sort -k2,2 -k3,3n | bgzip -c > dbsnp142.txt.bgz
+    #   tabix -s 2 -b 3 -e 3 dbsnp142.txt.bgz
+    DBSNP_FILE=os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'dbsnp144.txt.bgz')
+
+class InpsyghtConfig(BaseConfig):
+    BROWSER_NAME = 'InPSYght'
+    DATASET_NAME = 'InPSYght'
+    NUM_SAMPLES = 735
+
+    MONGO = {
+        'host': 'localhost',
+        'port': 27017,
+        'name': 'inpsyght',
+    }
+
+    BASE_COVERAGE = [
+        {
+            'min-length-bp': 0,
+            'max-length-bp': 5000,
+            'path': {str(chrom): '/var/browser_coverage/inpsyght/full/{chrom}.inpsyght.coverage.json.gz'.format(chrom=chrom) for chrom in range(1,1+22)},
+        },
+        {
+            'min-length-bp': 5000,
+            'max-length-bp': sys.maxint,
+            'path': {str(chrom): '/var/browser_coverage/inpsyght/bins/{chrom}.inpsyght.coverage.bins.json.gz'.format(chrom=chrom) for chrom in range(1,1+22)},
+        },
+    ]
+
+    _FILES_DIRECTORY = '/var/imported/inpsyght'
     SITES_VCFS = glob.glob(os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'ALL.polymorphic.topmed_freeze2.vcf.gz')),
     GENCODE_GTF = os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'gencode.gtf.gz'),
     CANONICAL_TRANSCRIPT_FILE = os.path.join(os.path.dirname(__file__), _FILES_DIRECTORY, 'canonical_transcripts.txt.gz'),
